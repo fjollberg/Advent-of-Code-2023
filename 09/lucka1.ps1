72a9b2b4c7a7e55d52762874d0ca8c459cdf6c28
Fredrik Jönsson <fjollberg@gmail.com>
2023-12-09 09:36:15 +0100

    Day 9, this was easy, but I made a slightly different stop criteria.

---
+++ 09/lucka1.ps1
@@ -0,0 +1,37 @@
+[CmdletBinding()]
+param(
+    [ValidateNotNull()]
+    [String]$Path,
+
+    [int]$Stop = 0
+)
+
+$Data = Get-Content $Path
+
+function Get-Next {
+    param (
+        [int[]]$Sequence
+    )
+
+    Write-Debug ("Sequence: {0}" -f ($Sequence -join " "))
+
+    if (($Sequence | Sort-Object -Unique).Count -eq 1) {
+        $Sequence[0]
+    } else {
+        $NewSequence = for ([int]$i = 1; $i -lt $Sequence.Length; $i++) {
+            $Sequence[$i] - $Sequence[$i-1]
+        }
+        $Sequence[$Sequence.Length - 1] + (Get-Next $NewSequence)
+    }
+}
+
+foreach ($line in $Data) {
+    $Next = Get-Next ($line -split " +")
+    Write-Debug ("line: {0}, next {1}" -f (1+$i++), $Next)
+    $Sum += $Next
+    if ($Stop -and $i -gt $Stop) {
+        break
+    }
+}
+
+$Sum
